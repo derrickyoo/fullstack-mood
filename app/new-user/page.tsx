48990424639ea1a34e635c4e28f647afe8d91be7
Derrick Yoo <derrickyoo@gmail.com>
2023-07-04 15:18:09 -0700

    Add new user page

--- app/new-user/page.tsx
+++ app/new-user/page.tsx
@@ -1,5 +1,35 @@
-function NewUserPage() {
-  return <div>New user page</div>;
+import { prisma } from "@/utils/db";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+async function createNewUser() {
+  const user = await currentUser();
+
+  if (user) {
+    const match = await prisma.user.findUnique({
+      where: {
+        clerkId: user.id,
+      },
+    });
+
+    if (!match) {
+      await prisma.user.create({
+        data: {
+          clerkId: user.id,
+          email: user.emailAddresses[0].emailAddress,
+        },
+      });
+    }
+
+    redirect("/journal");
+  } else {
+    throw new Error("Impossible condition with Clerk middleware in place");
+  }
+}
+
+async function NewUserPage() {
+  await createNewUser();
+  return <div>Loading...</div>;
 }
 
 export default NewUserPage;
