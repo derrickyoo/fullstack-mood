a473364be5ac593078491bd5df9ee823f5149530
Derrick Yoo <derrickyoo@gmail.com>
2023-07-04 16:01:32 -0700

    Update dashboard segment layout

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,5 +1,15 @@
 function DashboardLayout({ children }: { children: React.ReactNode }) {
-  return <div>{children}</div>;
+  return (
+    <div className="h-screen w-screen relative">
+      <aside className="w-[200px] absolute top-0 left-0 h-full border-r border-black/10">
+        Mood
+      </aside>
+      <div className="ml-[200px]">
+        <header className="h-[60px] border-b border-black/10">hello</header>
+        <div>{children}</div>
+      </div>
+    </div>
+  );
 }
 
 export default DashboardLayout;
