a7bddb99ecec29a05eb50deb055d9c998803f670
Derrick Yoo <derrickyoo@gmail.com>
2023-07-04 11:06:52 -0700

    Add homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,21 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
+      <div className="w-full max-w-[600px] mx-auto">
+        <h1 className="text-6xl mb-4">Fullstack Mood</h1>
+        <p className="2xl text-white/60 mb-4">
+          Built using the latest and greatest in Next.js Version 13
+        </p>
+        <div>
+          <Link href="/journal">
+            <button className="bg-pink-600 px-4 py-2 rounded-lg text-xl">
+              Get started
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
