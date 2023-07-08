2521fb6e70ca15623c0938ae5090b0f0b0f7ac8c
Derrick Yoo <derrickyoo@gmail.com>
2023-07-08 11:07:43 -0700

    Add PATCH to api util

--- utils/api.ts
+++ utils/api.ts
@@ -2,6 +2,20 @@ function createURL(path: string) {
   return `${window.location.origin}/${path}`;
 }
 
+export async function updateEntry(id: string, content: string) {
+  const res = await fetch(
+    new Request(createURL(`/api.journal/${id}`), {
+      method: "PATCH",
+      body: JSON.stringify({ content }),
+    })
+  );
+
+  if (res.ok) {
+    const data = await res.json();
+    return data.data;
+  }
+}
+
 export async function createNewEntry() {
   // Next.js fetch uses Request web standard (see MDN)
   const res = await fetch(
