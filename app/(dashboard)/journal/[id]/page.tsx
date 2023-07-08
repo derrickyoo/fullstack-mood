9cf12563e4d24d994ff7dc3cc98893ef3759ee17
Derrick Yoo <derrickyoo@gmail.com>
2023-07-08 10:46:30 -0700

    Pass in serializble data from server component to client component

--- app/(dashboard)/journal/[id]/page.tsx
+++ app/(dashboard)/journal/[id]/page.tsx
@@ -1,16 +1,34 @@
 import EntryEditor from "@/app/components/EntryEditor";
+import getUserByClerkId from "@/utils/auth";
+import { prisma } from "@/utils/db";
 
-function JournalEntryPage({
+async function getEntry(id: string) {
+  const user = await getUserByClerkId();
+  const entry = await prisma.journalEntry.findUnique({
+    where: {
+      userId_id: {
+        userId: user.id,
+        id,
+      },
+    },
+  });
+
+  return entry;
+}
+
+async function JournalEntryPage({
   params,
 }: {
   params: {
     id: string;
   };
 }) {
+  const entry = await getEntry(params.id);
+
   return (
     <div>
       {params.id}
-      <EntryEditor />
+      <EntryEditor entry={entry} />
     </div>
   );
 }
